5af63c661b772eb97cfdb1929061035e31a32fec
jojowil <jojowil@gmail.com>
2026-01-08 11:41:53 -0500

    cleanup chapter 3 files.

--- Chapter Source Code/Chapter 3 – Input and Output/output.c
+++ Chapter Source Code/Chapter 3 – Input and Output/output.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char *argv[]) {
-    char c;
     char name[30];
-
     snprintf(name, sizeof name, "%s", "John Smith");
-    c = 'L';
 
+    char c = 'L';
     fputc(c, stdout);
     fputc('\n', stdout);
 
