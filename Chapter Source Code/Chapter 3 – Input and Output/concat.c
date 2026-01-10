d538b69ead4a843cb9a485973c22767392427aed
bill <jojowil@gmail.com>
2026-01-10 17:30:46 -0500

    chapter 3 code changes

--- Chapter Source Code/Chapter 3 – Input and Output/concat.c
+++ Chapter Source Code/Chapter 3 – Input and Output/concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
     // an initialized string (compile-time).
     char astring[] = "A string of characters";
 
